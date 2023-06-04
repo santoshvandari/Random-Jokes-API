e9565fec9b4e04f3342f879ed0f43eb2a3d30f71
santoshvandari <santoshvandari1@gmail.com>
2023-06-04 07:09:31 +0545

    updated

--- JokesAPI.php
+++ JokesAPI.php
@@ -8,6 +8,10 @@ if($_SERVER["REQUEST_METHOD"]=="GET"){
     include("JokesList.php");
     header("Content-Type:Application/JSON");
     $joke=array("joke"=>$jokes[array_rand($jokes)]);
+    header("Content-Type:Application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
     $response = json_encode($joke);
     echo $response;
 }else{
