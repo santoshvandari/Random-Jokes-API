7cd1bbf110714efcc3d5195ddbc9a67c8aec987a
santoshvandari <santoshvandari1@gmail.com>
2023-06-07 07:20:26 +0545

    Some File Modified

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ function FetchData(){
     fetch(url)
     .then(response=>response.json())
     .then(response=>{
-        console.log(response);
         document.querySelector("#fetch-result").innerHTML=JSON.stringify(response);
     }).catch(e=>{
         console.log(e);
