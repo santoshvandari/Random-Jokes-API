877357663065cf9f50c6a20f5d50e0609ad684b8
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-16 21:11:03 +0545

    Updated

--- JokesAPI/index.php
+++ JokesAPI/index.php
@@ -1,17 +1,33 @@
 <?php
 
-// if($_SERVER["REQUEST_METHOD"]=="GET"){
-//     include("JokesList.php");
-//     $joke=array("joke"=>$jokes[array_rand($jokes)]);
-//     header("Content-Type:Application/json");
-//     header("Access-Control-Allow-Origin: *");
-//     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-//     header("Access-Control-Allow-Headers: Content-Type");
-//     $response = json_encode($joke);
-//     echo $response;
-// }else{
-//     echo "Failed to Load Data";
-// }
+if($_SERVER["REQUEST_METHOD"]=="GET"){
+    // include("JokesList.php");
+    // $joke=array("joke"=>$jokes[array_rand($jokes)]);
+    header("Content-Type:Application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+
+
+    $filename="jokes.txt";
+        // Read the file contents
+        $fileContents = file_get_contents($filename);
+        $jokes = explode('"', $fileContents);
+        // Output the contents
+        // echo $fileContents;
+        // $jokes=array_filter(" ",$jokes);
+        $joke=array("joke"=>$jokes[array_rand($jokes)]);
+        
+        print_r($joke);
+
+
+
+
+    $response = json_encode($joke);
+    // echo $response;
+}else{
+    echo "Failed to Load Data";
+}
 
 
 
@@ -22,6 +38,7 @@
 
 
 
+;
 
 
 
@@ -30,16 +47,17 @@
 
 
 
-
-$filename = 'example.txt'; // Replace with your file's name
+$filename = 'jokes.txt'; // Replace with your file's name
 
 // Check if the file exists
 if (file_exists($filename)) {
     // Read the file contents
     $fileContents = file_get_contents($filename);
-
+    $dataArray = explode('"', $fileContents);
     // Output the contents
-    echo $fileContents;
+    // echo $fileContents;
+    print_r($dataArray);
+    echo $dataArray[3];
 } else {
     echo "File not found.";
 }
