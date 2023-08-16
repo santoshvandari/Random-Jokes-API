fe78c9c800b73caeddb4c3d3dd2c063d198c3109
Santosh Bhandari <santoshvandari1@gmail.com>
2023-08-16 20:58:48 +0545

    Updated

--- JokesAPI/index.php
+++ JokesAPI/index.php
@@ -1,14 +1,46 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="GET"){
-    include("JokesList.php");
-    $joke=array("joke"=>$jokes[array_rand($jokes)]);
-    header("Content-Type:Application/json");
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type");
-    $response = json_encode($joke);
-    echo $response;
-}else{
-    echo "Failed to Load Data";
+
+// if($_SERVER["REQUEST_METHOD"]=="GET"){
+//     include("JokesList.php");
+//     $joke=array("joke"=>$jokes[array_rand($jokes)]);
+//     header("Content-Type:Application/json");
+//     header("Access-Control-Allow-Origin: *");
+//     header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+//     header("Access-Control-Allow-Headers: Content-Type");
+//     $response = json_encode($joke);
+//     echo $response;
+// }else{
+//     echo "Failed to Load Data";
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$filename = 'example.txt'; // Replace with your file's name
+
+// Check if the file exists
+if (file_exists($filename)) {
+    // Read the file contents
+    $fileContents = file_get_contents($filename);
+
+    // Output the contents
+    echo $fileContents;
+} else {
+    echo "File not found.";
 }
 ?>
